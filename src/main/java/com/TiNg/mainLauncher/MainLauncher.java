3dad69c13845b0f9a1ee6983fddaa88dd37821a7
bin33 <122081846@qq.com>
2020-10-07 23:04:39 +0800

    V0.0.0

---
+++ src/main/java/com/TiNg/mainLauncher/MainLauncher.java
@@ -0,0 +1,7 @@
+package com.TiNg.mainLauncher;
+
+public class MainLauncher {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/com/TiNg/windows/FirstWindow.java
@@ -0,0 +1,4 @@
+package com.TiNg.windows;
+
+public class FirstWindow {
+}
