af62c3c0acd3bac45255b294e8171ec213332ba4
bin33 <122081846@qq.com>
2020-10-09 21:23:13 +0800

    V0.0.2  转fxml文件实现

--- src/main/java/com/TiNg/windows/FirstWindow.java
+++ src/main/java/com/TiNg/windows/FirstWindow.java
@@ -5,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class FirstWindow extends Stage {
     AnchorPane anchorPane = new AnchorPane();
     Scene scene = new Scene(anchorPane);
@@ -16,12 +18,21 @@ public class FirstWindow extends Stage {
         setTitle("New_ModbusTool -ver0.0.0");
         setWidth(windowWidth);
         setHeight(windowHeight);
+        setResizable(false);
         anchorPane.setStyle("-fx-background-color:#a5a3a3");
         setScene(scene);
 
-        COMConnect comConnect = new COMConnect(windowWidth);
+/*        COMConnect comConnect = new COMConnect(windowWidth);
         anchorPane.getChildren().add(comConnect);
-        AnchorPane.setBottomAnchor(comConnect,0.0);
+        AnchorPane.setBottomAnchor(comConnect,0.0);*/
+
+        try {
+            COMConnect comConnect1 = new COMConnect(windowWidth);
+            anchorPane.getChildren().add(comConnect1);
+            AnchorPane.setBottomAnchor(comConnect1,0.0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         show();
     }
 }
